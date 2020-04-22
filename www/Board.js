4a6ebdebe3ffb454cf6e6046f0c2dd9d9be80638
DouglasKarlberg <DouglasKarlberg@protonmail.com>
2020-04-22 21:58:41 +0200

    Added constructor + empty functions from API

--- www/Board.js
+++ www/Board.js
@@ -1,4 +1,36 @@
-class Board { }
+class Board {
+    
+    constructor(game){
+        this.game = game;
+        this.matrix = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ]
+        this.currentPlayer = 1;
+        this.playInProgress = false;
+
+        this.addEventListener();
+        this.render();
+        //not done -- missing tellTurn from Game
+    }
+
+    async makeMove(column){}
+
+    wincCheck(){}
+
+    render(){}
+
+    markWin(combo){}
+
+    addEventListener(){}
+
+    removeEventListener(){}
+
+}
 
 // make it possible to test on backend
 if (typeof global !== 'undefined') { global.Board = Board };
