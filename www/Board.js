24a4a39b865562ac9283519fd2f6713e954e70ec
sul77 <mohsul01@gmail.com>
2020-04-23 12:49:07 +0200

    wrote code game.js and board.js

--- www/Board.js
+++ www/Board.js
@@ -1,4 +1,29 @@
-class Board { }
+class Board {
+    constructor(game) {
+        this.ROWS = 6;
+        this.COLS = 7;
+        this.currentPlayer = 1;
+        this.playInProgress = false;
+        this.createGrid();
+        this.game = game;
+    }
+
+    createGrid() {
+        const $board = $(this.game);
+
+        for (let row = 0; row < this.ROWS; row++) {
+            const $row = $('<div>').addClass('row');
+            $board.append($row);
+            for (let col = 0; col < this.COLS; col++)
+                const $col = $('<div').addClass('col empty');
+            $row.append($col);
+        }
+        $board.append($row);
+        console.log($board.html());
+    }
+}
 
 // make it possible to test on backend
-if (typeof global !== 'undefined') { global.Board = Board };
+if (typeof global !== 'undefined') {
+    global.Board = Board
+};
--- www/Game.js
+++ www/Game.js
@@ -1,4 +1,19 @@
-class Game { }
+class Game {
+
+    constructor() {
+        this.addEventListener();
+        this.start();
+
+    }
+
+    start() {}
+    tellTurn(player) {}
+    over(won)
+    addEventListener();
+}
+const board = new Board('#board');
 
 // make it possible to test on backend
-if (typeof global !== 'undefined') { global.Game = Game };
+if (typeof global !== 'undefined') {
+    global.Game = Game
+};
