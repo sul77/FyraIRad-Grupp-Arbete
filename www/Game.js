e031af39153c1e82e1141aa1c10a431f30035bb5
sul77 <mohsul01@gmail.com>
2020-05-03 22:46:37 +0200

    tellturn function

--- www/Game.js
+++ www/Game.js
@@ -8,20 +8,32 @@ class Game {
     }
 
     start() {
-        this.board = new Board(this);
-    }
-    tellTurn(player) {
+            this.board = new Board(this);
+        }
+        /* tellTurn(player) {
 
-        if (player < 1 || player > 2 || player % 1 != 0){
-            throw (new Error("player must be 1 or 2"));
+             if (player < 1 || player > 2 || player % 1 != 0){
+                 throw (new Error("player must be 1 or 2"));
+             }
+             if (player === 1) {
+                 $('.message').textContent = "Röds tur...";
+             }
+             if (player === 2) {
+                 $('.message').textContent = "Guls tur...";
+             }
+             
+         }*/
+
+    tellTurn(player) {
+        if (player !== 1 && player !== 2) {
+
+            throw (new Error('player must be 1 or 2'));
         }
-        if (player === 1) {
-            $('.message').textContent = "Röds tur...";
-        }
-        if (player === 2) {
-            $('.message').textContent = "Guls tur...";
-        }
-        
+        let $message = $('.message');
+        $message.innerHTML = player === 1 ? 'Röds tur.' :
+            player === 2 ? 'Guls  tur.' :
+            "...";
+
     }
     over(won) {
 
